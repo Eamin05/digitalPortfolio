3073fcd2c9f102b463520a8915cc986b667c9de3
Eamin05 <eaminjawed@gmail.com>
2025-03-01 14:35:40 +0530

    Updated with contact form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
-import { dancingScript, playfairDisplay } from "./Fonts";
+import { dancingScript, playfairDisplay, poppins } from "./Fonts";
 import "../styles/Contact.scss";
 import { Button } from "antd";
 import Image from "next/image";
@@ -91,52 +91,52 @@ const Contact = () => {
       <div className="contact-part">
         <Image src={"/contact.png"} width={300} height={300} alt="code" />
       </div>
-      <div className="contact-part contact-form">
-        <h1 className={`${dancingScript.className}`}>Contact Me</h1>
+      <div className={`contact-part contact-form ${poppins.className}`}>
+        <h1>Contact Me</h1>
         <div className="">
           <form onSubmit={handleSubmit} className="">
             <div className="fields-container">
-              <div className={`${playfairDisplay.className} contact-fields`}>
-                <label className="">Name*</label>
+              <div className={`contact-fields`}>
                 <input
                   type="text"
                   value={formData.name}
                   onChange={(e) => handleChange("name", e?.target?.value)}
                   required
+                  placeholder="Your Name"
                 />
               </div>
-              <div className={`${playfairDisplay.className} contact-fields`}>
-                <label className="">Email*</label>
+              <div className={`contact-fields`}>
                 <input
                   type="email"
                   value={formData.email}
                   onChange={(e) => handleChange("email", e?.target?.value)}
                   required
                   className=""
+                  placeholder="Your Email"
                 />
               </div>
-              <div className={`${playfairDisplay.className} contact-fields`}>
-                <label className="">Subject*</label>
+              <div className={`contact-fields`}>
                 <input
-                  type="text"
+                  type="number"
                   value={formData.subject}
                   onChange={(e) => handleChange("subject", e?.target?.value)}
                   required
                   className=""
+                  placeholder="Phone Number"
                 />
               </div>
-              <div className={`${playfairDisplay.className} contact-fields`}>
-                <label className="">Message*</label>
+              <div className={`contact-fields`}>
                 <textarea
                   rows="6"
                   value={formData.message}
                   onChange={(e) => handleChange("message", e?.target?.value)}
                   required
                   className=""
+                  placeholder="Message"
                 ></textarea>
               </div>
             </div>
-            <Button htmlType="submit" type="primary">
+            <Button htmlType="submit" type="primary" className="submit-btn">
               {loading ? "Sending..." : "Send Message"}
             </Button>
           </form>
